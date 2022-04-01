785b35a122a68a0f6026f539b9d020efd4259b9e
lasini1022 <lkurukulasooriya@ryerson.cs>
2022-03-31 23:24:39 -0400

    added command cases

--- mem.java
+++ mem.java
@@ -11,38 +11,83 @@ public class mem {
 
     public static void main(String[] args) {
         
-        String email;
-        String password;
         // Scanner scanner = new Scanner(System.in);
         // System.out.println("Enter Number of Students")
         // int num = scanner.nextint();
         int num = 4;
         String[][] fileParts = new String[num][4];
-        try {
+        try 
+        {
             File accountFile = new File("User_Info.txt");
             Scanner reader = new Scanner(accountFile);
-            for (int i=0; reader.hasNextLine() && i<fileParts.length; i++) {
+            for (int i=0; reader.hasNextLine() && i<fileParts.length; i++) 
+            {
                 String [] parts = (reader.nextLine()).split(" ");
                 fileParts[i] = parts;
-                for(int j=0; j<fileParts[0].length; j++){
+                for(int j=0; j<fileParts[0].length; j++)
+                {
                     fileParts[i][j]=fileParts[i][j].replaceAll("_", " ");
                 }
             }
             reader.close();
-          }
-        catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) 
+        {
             System.out.println("File Not Found.");
-          }
+        }
         
         //Add the accounts from txt file to arraylist of Players and Staff
-        for (String[] element : fileParts) {
-            System.out.println(Arrays.toString(element));
-            if(element[0].equals("P")){
+        for (String[] element : fileParts) 
+        {
+            //System.out.println(Arrays.toString(element));
+            if(element[0].equals("P"))
+            {
                 studentList.add(new Player(element[1], element[2], element[3]));//(name, email, password) for Players
-              }
-            else if(element[0].equals("S")){
+            }
+            else if(element[0].equals("S"))
+            {
                 staffList.add(new Staff(element[1], element[2], element[3]));//(name, email, password) for Staff Members
-              }
-          }  
+            }
+        } 
+
+        //////////////////////////
+        Scanner scanner = new Scanner(System.in);
+		System.out.print(">");
+
+		while (scanner.hasNextLine())
+        {
+			String inputLine = scanner.nextLine();
+			if (inputLine == null || inputLine.equals("")) 
+			{
+				System.out.print("\n>");
+				continue;
+			}
+
+			Scanner commandLine = new Scanner(inputLine);
+			String action = commandLine.next();
+
+			if (action == null || action.equals("")) 
+			{
+				System.out.print("\n>");
+				continue;
+			}
+
+			else if (action.equalsIgnoreCase("Q") || action.equalsIgnoreCase("QUIT") || action.equals("3")){
+				commandLine.close();//quits the program
+				return;
+			}
+
+			else if (action.equalsIgnoreCase("REGISTER") || action.equalsIgnoreCase("R") || action.equals("1")){
+				//register code
+			}
+
+            else if (action.equalsIgnoreCase("LOG IN") || action.equalsIgnoreCase("L") || action.equals("2"))
+			{
+				//register code
+			}
+            System.out.print("\n>");
+			commandLine.close();
+		}
+		scanner.close();
     }
 }
