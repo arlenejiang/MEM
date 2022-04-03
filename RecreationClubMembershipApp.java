bf1aab9e8d4b7114e8118ac4fc0c3658fe79369e
ca-bee <natasha.narasimhan@gmail.com>
2022-04-03 18:53:40 -0400

    some edits

--- RecreationClubMembershipApp.java
+++ RecreationClubMembershipApp.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 
 public class RecreationClubMembershipApp {
     static Scanner in = new Scanner(System.in);
@@ -250,6 +254,7 @@ public class RecreationClubMembershipApp {
                 clearConsole();
                 System.out.println("*** Send Announcement ***\n");
                 // insert method for sending email through java code
+                sendAnnouncement(member.getEmail());
             // } catch (IOException e) {
             //     System.out.println(e.getMessage());
             // }
@@ -289,4 +294,49 @@ public class RecreationClubMembershipApp {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
+    public static void sendAnnouncement(String memberemail){
+      String to = "abcd@gmail.com";
+
+      // Sender's email ID needs to be mentioned
+      String from = memberemail;
+
+      // Assuming you are sending email from localhost
+      String host = "localhost";
+
+      // Get system properties
+      Properties properties = System.getProperties();
+
+      // Setup mail server
+      properties.setProperty("mail.smtp.host", host);
+
+      // Get the default Session object.
+      Session session = Session.getDefaultInstance(properties);
+
+      try {
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.addRecipients(Message.RecipientType.TO, new InternetAddress(to));
+
+         // Set Subject: header field
+         message.setSubject("This is the Subject Line!");
+
+         // Now set the actual message
+         message.setText("This is actual message");
+
+         // Send message
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      } catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+
+    }
+
 }
